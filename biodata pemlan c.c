78fcaddccead16ae924ca4ae786869d6cd532a01
NabilHabibi <nblhabibi55@gmail.com>
2020-09-16 17:11:19 +0700

    Menampilkan file commit

---
+++ biodata pemlan c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("\n================================================");
+    printf("\n");
+    printf("\tBIODATA PRIBADI MAHASISWA\n\n");
+    printf("Nama\t\t: Nabil Habibi\n");
+    printf("NPM\t\t: 19081010184\n");
+    printf("Angkatan\t: 2019\n");
+    printf("Jurusan\t\t: Teknik Informatika\n");
+    printf("Fakultas\t: Ilmu Komputer\n");
+    printf("Jenis Kelamin\t: Laki-laki\n");
+    printf("TTL\t\t: Sidoarjo,5 Mei 2001\n");
+    printf("Alamat\t\t: Perum Griya Taman Asri GD/25\n");
+    printf("Email\t\t: nblhabibi55@gmail.com\n");
+    printf("\n================================================");
+    
+return 0;
+}
